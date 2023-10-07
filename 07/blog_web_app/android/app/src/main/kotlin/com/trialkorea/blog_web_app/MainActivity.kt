9bbd2039c8db3274192220366251836cd4c3f58a
sonstein <sonstein@kakao.com>
2023-10-07 22:26:10 +0900

    사전 환경 설정

---
+++ 07/blog_web_app/android/app/src/main/kotlin/com/trialkorea/blog_web_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trialkorea.blog_web_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 07/blog_web_app/lib/screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  // const HomeScreen({Key? key}) : super(key: key)
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Home Screen')
+    );
+  }
+
+}
